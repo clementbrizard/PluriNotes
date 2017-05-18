0a515075daec796635cdb4c922bde4ae86b90621
Clément <clementbrizard53@gmail.com>
2017-05-18 18:36:58 +0200

    Modifs de syntaxe

--- plurinotes.h
+++ plurinotes.h
@@ -3,35 +3,24 @@
 
 #include <iostream>
 #include <string>
-#include <time.h>
 #include <fstream>
+#include "timing.h"
 
 using namespace std;
 
-
 //On crée les classes de base
-
-//Classe pour la date 
-class date {
-private:
-	int jour;
-	int mois;
-	int annee;
-public:
-	date(int jour = 0, int mois = 0, int annee = 0);
-};
 
 //Notes Exception pour les erreurs
 class NotesException{
 private:
 	string info;
 public:
-	NotesException(const string& message):info(message){}
+	NotesException(const string& message) : info(message){}
 	string getInfo() const { return info; }
 };
 
 //Classe Manager
-class Manager{
+template <class T> class Manager{
 private:
 	static T* m_uniqueInstance;
 	T* m_tab ;
@@ -39,98 +28,109 @@ private:
 	int m_nbMax;
 	string m_filename;
 	Manager();
-	Manager(nm : const Manager&);
+	Manager(const Manager& nm);
 	~Manager();
-	operator=(nm : const Manager&) : Manager&;
+	const Manager& operator=(const Manager& nm);
 public:
-	//Class iterator design pattern
-template <class T> class iterator{
-private:
-	T** m_current;
-	int m_remain;
-	iterator(T** current, int remain);
-public:
-	const bool isDone();
-	next();
-	const T& currentValue();
-}
-	int getNbArticles();
+	int getNb() {return m_nb; };
 	static Manager& giveInstance();
 	static freeInstance();
 	T& getNew(const int& id);
 	T& get(const int& id);
-	load (const string& f);
-	const save();
-	restore(const int& id);
+	void load (const string& f);
+	void save();
+	void restore(const int& id);
+		//Class iterator
+    template <class T> class iterator{
+    private:
+        T** m_current;
+        int m_remain;
+        iterator(T** current, int remain);
+    public:
+        const bool isDone();
+        void next();
+        const T& currentValue();
+    };
 	iterator begin();
 };
 
 //Class Note
-class article : public note{
+
+class Article : public Note{
 private:
 	string m_text;
-	public :
-	Article(const int& id,const string& title,const string&  text)
+public :
+	Article(const int& id, const string& title, const string& text) : Note(id,title), m_text(text){}
 	virtual ~Article();
-	setText(const string& text):m_text(text)
+	setText(const string& text) {m_text=text; }
 	const string& getText() const {return m_text;}
 	virtual show();
 
 };
-class task : public note{
+
+class Task : public Note{
 private:
 	string m_action;
 	int m_priority;
-	int m_deadline;
+	date m_deadline;
 public:
-	Task(const int& id,const string&  title,const string& action,const int& priority);
+	Task(const int& id, const string& title, const string& action, const int& priority, const Date& deadline=O) : Note(id, title), m_action(action), m_priority(priority), m_deadline(deadline)
 	virtual ~Task();
-	setAction(const string& action);
-	setPriority(const int& priority);
-	setDeadline(const int& deadline);
-	const string& getAction() const {return m_action;}  
+	setAction(const string& action) {m_action=action; }
+	setPriority(const int& priority) {m_priority=priority; }
+	setDeadline(const Date& deadline) {m_deadline = deadline; }
+	const string& getAction() const {return m_action;}
 	const int& getPriority() const {return m_priority;}
-	const int& getDeadline() const {return m_deadline;}
-	virtual show(); 
-};
-class media :public note{
-private:
-	string m_description; 
-	string m_imageFileName;
-public:
-	media(const string& id,const string&  title, const string&description,const string&  imageFileName);
-	virtual ~media()  const = 0;
-	setDescription(const string& description):m_description(description){}
-	setImageFilename(const string& imageFileName):m_imageFileName(imageFileName){}
-	const string& getDescription() const {return m_description;}
-	const string& getImageFilename() const{return m_imageFileName;}
+	const Date& getDeadline() const {return m_deadline;}
 	virtual show();
 };
-class image : public media{
-	public :
-	~image();
-};
-class video : public media{
-	public :
-	~video();
-};
-class audio : public media{
-	public :
-	~audio();
+
+class Media : public Note{
+private:
+	string m_description;
+	string m_imageFileName;
+public:
+	media(const string& id, const string& title, const string& description, const string& imageFileName) : Note(id,title), m_description(description), m_imageFilename(imageFilename){}
+	virtual ~media() const = 0;
+	setDescription(const string& description) { m_description=description; }
+	setImageFilename(const string& imageFileName){m_imageFileName=imageFileName; }
+	const string& getDescription() const {return m_description; }
+	const string& getImageFilename() const{return m_imageFileName; }
+	virtual show();
 };
 
-class couple{
+class Image : public Media{
+public :
+	Image(const string& id, const string& title, const string& description, const string& imageFileName) : Media(id,title,description,imageFileName){}
+	~Image();
+};
+
+class Video : public media{
+public :
+	Video(const string& id, const string& title, const string& description, const string& imageFileName) : Media(id,title,description,imageFileName){}
+	~Video();
+};
+
+class Audio : public media{
+public :
+    Audio(const string& id, const string& title, const string& description, const string& imageFileName) : Media(id,title,description,imageFileName){}
+    ~Audio();
+};
+
+class Couple{
 private:
-	Note m_referencingNote; 
+	Note m_referencingNote;
 	Note m_referencedNote;
 	string label;
 public:
-	couple(const Note& referencingNote,const Note& referencedNote,const string& label ='');
-	~couple();
-	setReferencingNote(const Note& referencingNote):m_referencingNote(referencingNote){}
-	setReferencedNote(const Note& referencedNote):m_referencedNote(referencedNote){}
-setLabel(const string& label):label(label){}
-const Note& getReferencingNote() const {return m_referencingNote;}
-const Note& getReferencedNote() const  {return m_referencedNote;}
-const string&  getLabel() const {return label;}
+	Couple(const Note& referencingNote,const Note& referencedNote,const string& label ='') : m_referencingNote(referencingNote), m_referencedNote(referencedNote) {}
+	~Couple();
+	setReferencingNote(const Note& referencingNote){m_referencingNote=referencingNote; }
+	setReferencedNote(const Note& referencedNote){m_referencedNote=referencedNote; }
+    setLabel(const string& label){m_label=label; }
+    const Note& getReferencingNote() const {return m_referencingNote; }
+    const Note& getReferencedNote() const  {return m_referencedNote; }
+    const string& getLabel() const {return label; }
 };
+
+
